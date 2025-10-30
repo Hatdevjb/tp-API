953a491e4cb7888d3d8f38236edb4a71096e0f46
jbAfpa <jb.vaugi@hotmail.fr>
2025-10-30 13:46:21 +0100

    js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,30 @@
 "use strict"
 
-const promesseFetch = fetch(`https://data.geopf.fr/geocodage/completion/?text=du texte&maximumResponses=15`)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`Erreur HTTP ${response.status}`);
-        }
-        return response.json(); // Convertit le corps de la réponse en JSON
-    })
-    .then(data => {
-        console.log('Données récupérées :', data);
-    })
-    .catch(error => {
-        console.error('Une erreur s\'est produite :', error.message);
-    });
+let address= "rue Gabriel Péri";
+
+const URL = `https://data.geopf.fr/geocodage/completion/?text=du${address}maximumResponses=15`;
+
+    fetch (URL)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Erreur HTTP ${response.status}`);
+
+            }
+            console.log(response);
+            return response.json(); // Convertit le corps de la réponse en JSON
+        })
+        .then(dataJSON => {
+            console.log(dataJSON);
+            console.log(dataJSON.results);
+            console.log(dataJSON.results[0]);
+            console.log(dataJSON.results[0].city);
+
+            let optionHTML = document.createElement("option");
+            optionHTML.setAttribute();
+            selectHTML.appendChild(optionHTML);
+        })
+        .catch(error => {
+            console.error('Une erreur s\'est produite :', error.message);
+            error.textContent = "Une erreuer s'est produite durant la recherche.";
+        });
 
