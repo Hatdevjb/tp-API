43217cc90f31958637f36822e29010a7748c9d3a
jbAfpa <jb.vaugi@hotmail.fr>
2025-10-30 16:38:53 +0100

    css ok + option simple ok

--- js/script.js
+++ js/script.js
@@ -19,12 +19,16 @@ const URL = `https://data.geopf.fr/geocodage/completion/?text=du${address}maximu
             console.log(dataJSON.results[0]);
             console.log(dataJSON.results[0].city);
 
+            let selectHTML = document.getElementById('resultHTML');
+
             let optionHTML = document.createElement("option");
-            optionHTML.setAttribute();
+            optionHTML.setAttribute("value",dataJSON.results[0].fulltext);
+            optionHTML.textContent = dataJSON.results[0].fulltext;
             selectHTML.appendChild(optionHTML);
+            selectHTML.style.display = 'block';
         })
+
         .catch(error => {
             console.error('Une erreur s\'est produite :', error.message);
             error.textContent = "Une erreuer s'est produite durant la recherche.";
         });
-
