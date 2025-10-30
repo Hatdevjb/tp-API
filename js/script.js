2f22c6ad2189a2ad8ca47f15aea54f02d37f1a77
jbAfpa <jb.vaugi@hotmail.fr>
2025-10-30 13:45:05 +0100

    html + css  fait

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log("Hello World !")
+"use strict"
+
+const promesseFetch = fetch(`https://data.geopf.fr/geocodage/completion/?text=du texte&maximumResponses=15`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Erreur HTTP ${response.status}`);
+        }
+        return response.json(); // Convertit le corps de la réponse en JSON
+    })
+    .then(data => {
+        console.log('Données récupérées :', data);
+    })
+    .catch(error => {
+        console.error('Une erreur s\'est produite :', error.message);
+    });
+
